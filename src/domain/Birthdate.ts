581175db27520a0452e4319a54b8c36ce5fcf09b
sa.cux <sa.cux.dev@gmail.com>
2025-03-18 23:50:41 +0100

    feat(BirthDate): handle Feb 29 birthdays in leap and non-leap years

--- src/domain/Birthdate.ts
+++ src/domain/Birthdate.ts
@@ -6,9 +6,36 @@ export class BirthDate {
   }
 
   isSameDay(otherDate: BirthDate): boolean {
+    if (this.isFeb29()) {
+      if (!otherDate.isLeapYear() && otherDate.isFeb28()) {
+        return true;
+      }
+    }
+
     return (
       this.date.getMonth() === otherDate.date.getMonth() &&
       this.date.getDate() === otherDate.date.getDate()
     );
+  }
+
+  private isLeapYear(): boolean {
+    const year = this.date.getFullYear();
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  }
+
+  private isFeb29(): boolean {
+    return this.getMonth() === 1 && this.getDay() === 29;
+  }
+
+  private isFeb28(): boolean {
+    return this.getMonth() === 1 && this.getDay() === 28;
+  }
+
+  getMonth(): number {
+    return this.date.getMonth();
+  }
+
+  getDay(): number {
+    return this.date.getDate();
   }
 }
