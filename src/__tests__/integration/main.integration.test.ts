9e69fcf7d9e862f0b455d901f63d99376ba524f7
sa.cux <sa.cux.dev@gmail.com>
2025-03-18 23:54:00 +0100

    test(main): add leap non-leap years feature integration tests

--- src/__tests__/integration/main.integration.test.ts
+++ src/__tests__/integration/main.integration.test.ts
@@ -12,7 +12,9 @@ afterEach(async () => {
   try {
     await fs.unlink(TEST_CSV_PATH);
   } catch (error) {
-    console.warn("Dinamically generated test CSV file was already deleted or not found.");
+    console.warn(
+      "Dinamically generated test CSV file was already deleted or not found."
+    );
   }
 });
 
@@ -56,5 +58,86 @@ describe("Integration Test for main()", () => {
     );
 
     consoleSpy.mockRestore();
+  });
+
+  it("should send birthday emails on Feb 28 for Feb 29 birthdays in a non-leap year", async () => {
+    await fs.writeFile(
+      TEST_CSV_PATH,
+      `last_name,first_name,date_of_birth,email\n` +
+        `Leap,Year,2000-02-29,leap.year@foobar.com\n`
+    );
+
+    const nonLeapYearFeb28Timestamp = new Date("2025-02-28T00:00:00.000Z");
+
+    jest
+      .spyOn(global, "Date")
+      .mockImplementation(() => nonLeapYearFeb28Timestamp);
+
+    const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+
+    await main(TEST_CSV_PATH);
+
+    expect(consoleSpy).toHaveBeenCalledWith(
+      expect.stringContaining("Mock Email Sent")
+    );
+    expect(consoleSpy).toHaveBeenCalledWith(
+      expect.stringContaining("To: leap.year@foobar.com")
+    );
+
+    consoleSpy.mockRestore();
+    jest.restoreAllMocks();
+  });
+
+  it("should send birthday emails on Feb 29 for Feb 29 birthdays in a leap year", async () => {
+    await fs.writeFile(
+      TEST_CSV_PATH,
+      `last_name,first_name,date_of_birth,email\n` +
+        `Leap,Year,2000-02-29,leap.year@foobar.com\n`
+    );
+
+    const leapYearFeb28Timestamp = new Date("2024-02-29T00:00:00.000Z");
+
+    jest.spyOn(global, "Date").mockImplementation(() => leapYearFeb28Timestamp);
+
+    const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+
+    await main(TEST_CSV_PATH);
+
+    expect(consoleSpy).toHaveBeenCalledWith(
+      expect.stringContaining("Mock Email Sent")
+    );
+    expect(consoleSpy).toHaveBeenCalledWith(
+      expect.stringContaining("To: leap.year@foobar.com")
+    );
+
+    consoleSpy.mockRestore();
+    jest.restoreAllMocks();
+  });
+
+  it("should NOT send birthday emails on Feb 28 for Feb 29 birthdays in a leap year", async () => {
+    await fs.writeFile(
+      TEST_CSV_PATH,
+      `last_name,first_name,date_of_birth,email\n` +
+        `Leap,Year,2000-02-29,leap.year@foobar.com\n`
+    );
+
+    const leapYearFeb28Timestamp = new Date(
+      "2024-02-28T00:00:00.000Z"
+    ).getTime();
+    jest.spyOn(global.Date, "now").mockReturnValue(leapYearFeb28Timestamp);
+
+    const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+
+    await main(TEST_CSV_PATH);
+
+    expect(consoleSpy).not.toHaveBeenCalledWith(
+      expect.stringContaining("Mock Email Sent")
+    );
+    expect(consoleSpy).not.toHaveBeenCalledWith(
+      expect.stringContaining("To: leap.year@foobar.com")
+    );
+
+    consoleSpy.mockRestore();
+    jest.restoreAllMocks();
   });
 });
